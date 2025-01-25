5b3e97b307b47f45e2771391423e42c6da3209f9
Jan Kevin <jankevingerona15@gmail.com>
2025-01-25 09:59:22 +0800

    DashboardInitial

---
+++ src/main/java/com/frontend/Dashboard.java
@@ -0,0 +1,24 @@
+package com.frontend;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Dashboard extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("DashboardStyle.css").toExternalForm());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+     public static void main(String[] args) {
+        launch(args);
+     }
+
+}
