7c9a2e174866f23bdd1e8b6635c82a84f4570993
vergeldarjuan <162280667+vergeldarjuan@users.noreply.github.com>
2025-01-29 03:01:34 +0800

    Update quiz_system_schema.sql

--- lib/quiz_system_schema.sql
+++ lib/quiz_system_schema.sql
@@ -1,19 +1,16 @@
-CREATE DATABASE QuizSystem;
-USE QuizSystem;
-
-CREATE TABLE user (
+CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_account VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE quiz (
     quiz_id INT AUTO_INCREMENT PRIMARY KEY,
     quiz_user_score INT NOT NULL,
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE quiz_question (
