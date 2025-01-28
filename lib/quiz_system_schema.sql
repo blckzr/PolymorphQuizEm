1c3181f154b9f275ed22c2203773bdb2f3e4e6d3
vergeldarjuan <162280667+vergeldarjuan@users.noreply.github.com>
2025-01-28 17:11:54 +0800

    Create quiz_system_schema.sql

---
+++ lib/quiz_system_schema.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE QuizSystem;
+USE QuizSystem;
+
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_account VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE quiz (
+    quiz_id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_user_score INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE quiz_question (
+    question_id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_id INT NOT NULL,
+    question_answer VARCHAR(255) NOT NULL,
+    question VARCHAR(255) NOT NULL,
+    FOREIGN KEY (quiz_id) REFERENCES quiz(quiz_id)
+);
+
+CREATE TABLE quiz_category (
+    question_id INT NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    PRIMARY KEY (question_id, category),
+    FOREIGN KEY (question_id) REFERENCES quiz_question(question_id)
+);
+
+CREATE TABLE quiz_choice (
+    question_id INT NOT NULL,
+    correct_answer VARCHAR(255) NOT NULL,
+    question_answer VARCHAR(255) NOT NULL,
+    question_choices VARCHAR(255) NOT NULL,
+    PRIMARY KEY (question_id, question_answer),
+    FOREIGN KEY (question_id) REFERENCES quiz_question(question_id)
+);
